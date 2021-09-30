3f3c0fab94fd393941ae78b47e85398f709e695b
Nec2005 <necatiunal2005@gmail.com>
2021-09-30 13:19:10 +0100

    my first app with flask

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,23 +1,26 @@
-from logging import debug
 from flask import Flask
 
-app = Flask(__name__)
+app =  Flask(__name__)
 
-@app.route('/')
+@app.route('/') #decorator
+
 def hello():
-    return "Hello World from Flask!!!"
+    return 'Hello World!!!'
 
-@app.route('/second')
+@app.route("/second")
+
 def second():
-    return "Bize Her Yer Trabzon"
+    return "This is the second page"
+    
+@app.route("/third/subthird")
 
-@app.route('/third/subthird')
 def third():
     return "This is the subpage of third page"
 
-@app.route('/forth/<string:id>')
+@app.route("/forth/<string:id>")
 def forth(id):
-    return f'Id number of this page is {id}'
+    return f"Id of this page is {id}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def head():
+    return render_template("index.html", number1 = 34, number2 = 15)
+
+@app.route("/karil")
+def number():
+    num1, num2 = 23, 35
+    return render_template("body.html", value1 = num1, value2 = num2, value3 = num1 * num2)
+
+if __name__ == "__main__":
+    app.run(debug=True)
