97c15723f28df06a6e42d6face2bbecfb749f1d8
Nec2005 <necatiunal2005@gmail.com>
2021-10-06 18:40:43 +0100

    hands-on

---
+++ python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
@@ -0,0 +1,21 @@
+from os import rename
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    first = "This is my first conditions experience"
+    return render_template("index.html", message = first)
+
+@app.route('/tomas')
+def mylist():
+    names = ['Tom', 'John', 'Jane', 'Tomas', 'Nico']
+    return render_template("body.html", object = names)
+
+
+
+
+if __name__ == "__main__" :
+    # app.run(debug=True)
+    app.run(host = '0.0.0.0', port = 80)
